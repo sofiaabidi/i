6823289c6eca6c0dd3baa0447ada978f0d7fdb6f
sofiaabidi <sofiaabidi2005@gmail.com>
2026-02-07 15:26:47 +0530

    update gitignore

--- frontend/src/DesignCourses.tsx
+++
@@ -1,8 +0,0 @@
-import { useParams } from "react-router-dom";
-import DCApp from "./pages/dc/src/app/App";
-import "./pages/dc/src/styles/index.css";
-
-export default function DesignCourseApp() {
-  const { courseId } = useParams<{ courseId: string }>();
-  return <DCApp courseId={courseId || ""} />;
-}
